ce5f3571ae04004b6ecbf0b822ab242cc917e7d4
Ivan Rivas <ivanrivas@moderno.local>
2023-02-08 14:14:43 -0600

    aja

--- nextjs_practica/src/context/taskContext.js
+++ nextjs_practica/src/context/taskContext.js
@@ -1,7 +1,7 @@
 //Esta aplicacion necesita crear, enlista, eliminar y actualizar tareas, este archivo estará encargado de de llevar ese
 //estado de las tareas.
 
-//
+//no se que esta pasando aqui
 import { children, createContext, useContext } from "react";
 
 //este objeto lo voy a poder importar y exportar
@@ -16,7 +16,7 @@ export const useTask = () => { //esta funcion siver para no tener que importar e
 
 //se exporta el TaskProvider una funcion que contiene a otros
 export const TaskProvider = ({children}) => { //cualquier componente que me pasen al task provider puede acceder a las otras lineas de código
-    return <TaskContext.Provider>{children}</TaskContext.Provider>; //cualquier componente hijo va acceder a la variable que esta dentro de las llaves
+   return<TaskContext.Provider>{children}</TaskContext.Provider>; //cualquier componente hijo va acceder a la variable que esta dentro de las llaves
 };
 
 //¿Que hace el task provider?
