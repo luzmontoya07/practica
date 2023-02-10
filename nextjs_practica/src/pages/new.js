3552edd732eeaf2f3bacfaa1bb9aba94acb831fd
Luz Montoya Mac <luz.montoya@unah.hn>
2023-02-10 13:58:44 -0600

    se arregló el diseño y se puede editar pero toadvìa no se puede eliminar

--- nextjs_practica/src/pages/new.js
+++ nextjs_practica/src/pages/new.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Layout from "../components/Layout"
 import { useTask } from "../context/taskContext"
 import { useRouter } from "next/router"
@@ -9,8 +9,8 @@ const TaskFormPage = () => {
         titulo: "",
         descripcion: "",
     });
-    const {createTask} = useTask()
-     const { push } = useRouter()
+    const {createTask, updateTask, tasks} = useTask()
+     const { push, query } = useRouter()
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -22,26 +22,44 @@ const TaskFormPage = () => {
     const handleSubmit = e => {
 
         e.preventDefault() //evita que se refresque la pagina
-        createTask(task.titulo, task.descripcion) //crea la tarea
-        push('/')
+        
+
+        if(!query.id) {
+            createTask(task.titulo, task.descripcion) //crea la tarea
+        } else {
+            updateTask(query.id, task)
+
+        }
+        // push('/')
     }
+
+    useEffect(() => {
+        if(query.id){
+            const taskEcontrada = tasks.find(task => task.id === query.id)
+            setTask ({title: taskEcontrada.titulo, descripcion: taskEcontrada.descripcion})
+        }
+    },[]);
 
     return (
         <Layout>
             <form onSubmit={handleSubmit}>
-                <h1>Agregar tarea</h1>
+                <h1>{query.id ? "Editar tarea" : "Crear tarea"}</h1>  {/* si trae un id lo condiciona para que tenga un titulo distinto */}
 
                 <input type="text"
                     name="titulo"
                     placeholder='Escriba el titulo'
                     className="bg-gray-800 focus:text-gray-100 focus:outline-none w-full py-3 px-4 mb-5"
-                    onChange={handleChange} />
+                    onChange={handleChange} 
+                    value={task.titulo}
+                    />
 
                 <textarea rows="2"
                     placeholder="Coloque la descripción"
                     className="bg-gray-800 focus:text-gray-100 focus:outline-none w-full py-3 px-4 mb-5"
                     name="descripcion"
-                    onChange={handleChange}></textarea>
+                    onChange={handleChange}
+                    value={task.descripcion}
+                    ></textarea>
 
                 <button
                  className="bg-green-500 hover:bg-green-400 px-4 py-2 rounded-sm disabled:opacity-30"
